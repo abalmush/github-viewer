c4e8fdf3446c5f43a4ef5252bbe04a53c38afafe
Andrei Balmus <abalmush@gmail.com>
2021-06-17 22:57:25 +0300

    Integrating GraphQL and Apollo client

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { withApollo } from './container';
+import { FETCH_REPOS_QUERY } from './queries';
 
 function App() {
+  const { loading, error, data } = useQuery(FETCH_REPOS_QUERY, {
+    variables: {
+      login: process.env.REACT_APP_GITHUB_USER || ''
+    }
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
   return (
     <div className="App">
-      
+      {JSON.stringify(data)}
     </div>
   );
 }
 
-export default App;
+export default withApollo(App);
---
+++ src/queries/github.ts
@@ -0,0 +1,22 @@
+import { gql } from '@apollo/client';
+
+export const FETCH_REPOS_QUERY = gql`
+  query User ($login: String! $cursor: String) {
+    user(login: $login) {
+      id
+      repositories(first: 10 after: $cursor) {
+        totalCount
+        edges {
+          cursor
+          node {
+            id
+            name
+            stargazerCount
+            forkCount
+            description
+          }
+        }
+      }
+    }
+  }
+`;
